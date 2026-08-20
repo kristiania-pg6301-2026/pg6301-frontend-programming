5e02176ab8b4d6878732673ff0b4b1663eed12d4
Johannes Brodwall <jhannes@gmail.com>
2026-08-20 10:03:16 +0200

    🔵 refactor and reformat code
    * `npm install --save-dev prettier husky`
    * `npm husky init`
    * `npm pkg set scripts.test="prettier --check ."`

--- leapYears.test.js
+++ leapYears.test.js
@@ -1,12 +1,15 @@
-import {test, expect} from "vitest";
+import { test, expect } from "vitest";
 
-function isLeapYear(number) {
-    if (number % 100 === 0) return false;
-    return number % 4 === 0;
+function isLeapYear(year) {
+  if (year % 100 === 0) return false;
+  return year % 4 === 0;
 }
 
-test("that normal years are not leap years", () => expect(isLeapYear(2026)).toBe(false));
+test("that normal years are not leap years", () =>
+  expect(isLeapYear(2026)).toBe(false));
 
-test("that years divisible by four are leap years", () => expect(isLeapYear(2028)).toBe(true));
+test("that years divisible by four are leap years", () =>
+  expect(isLeapYear(2028)).toBe(true));
 
-test("that years divisible by 100 are not leap years", () => expect(isLeapYear(2100)).toBe(false));
+test("that years divisible by 100 are not leap years", () =>
+  expect(isLeapYear(2100)).toBe(false));
