42205dbbf8bd55184ad76dc05127c4cd7fc5d04d
Johannes Brodwall <jhannes@gmail.com>
2026-08-20 09:57:16 +0200

    🟢 test that most years are not leap years

--- leapYears.test.js
+++ leapYears.test.js
@@ -1,3 +1,7 @@
 import {test, expect} from "vitest";
 
+function isLeapYear(number) {
+    return false;
+}
+
 test("that normal years are not leap years", () => expect(isLeapYear(2026)).toBe(false));
