1c5977859e4ffd4c364df01d6e978c94ff43c428
Johannes Brodwall <jhannes@gmail.com>
2026-08-20 10:30:21 +0200

    🟢 minefield with multiple rows

--- minefield.test.js
+++ minefield.test.js
@@ -1,7 +1,11 @@
 import { test, expect } from "vitest";
 
 function showMinefield(minefield) {
-  return ["0"];
+  let hints = [];
+  for (const row in minefield) {
+    hints.push("0");
+  }
+  return hints;
 }
 
 test("empty minefield", () => {
