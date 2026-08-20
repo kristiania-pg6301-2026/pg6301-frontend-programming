9f2366ae16784b8cc88c2b9e374b7e4b1812be53
Johannes Brodwall <jhannes@gmail.com>
2026-08-20 10:57:51 +0200

    🟣 add typescript support
    * `npm install --save-dev typescript`
    * `npm tsc --init`
    * `npm pkg set scripts.test="tsc --noEmit && prettier --check ."`
    * rename `leapYear.test.{js => ts}`

--- leapYears.test.ts
+++ leapYears.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "vitest";
 
-function isLeapYear(year) {
+function isLeapYear(year: number) {
   if (year % 400 === 0) return true;
   if (year % 100 === 0) return false;
   return year % 4 === 0;
