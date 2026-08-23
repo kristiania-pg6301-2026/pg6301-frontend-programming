a91a4bc35ef3476d118d8361d70f4779cb5033ad
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 20:33:39 +0200

    🟢 test general error message with english

--- applicationTexts.test.ts
+++ applicationTexts.test.ts
@@ -1,10 +1,15 @@
 import { test, expect } from "vitest";
 
 const norwegian = {};
+const english = {};
 function applicationMessage(language: any, error: string) {
+  if (language === english) return "An error has occurred";
   return "Noe gikk feil";
 }
 
 test("general error message", () => {
   expect(applicationMessage(norwegian, "generalError")).toBe("Noe gikk feil");
+  expect(applicationMessage(english, "generalError")).toBe(
+    "An error has occurred",
+  );
 });
