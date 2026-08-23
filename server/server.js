6389fedf76e6e91d288cebfd492af31e004f8287
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 23:36:43 +0200

    update on server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 const app = new Hono();
 const tasks = [
@@ -13,12 +14,12 @@ app.get("/api/tasks", (c) => c.json(tasks));
 app.post("/api/tasks", async (c) => {
   const task = await c.req.json();
   tasks.push({ ...task, id: tasks.length });
-  return c.status(200);
+  return c.newResponse(null, 200);
 });
 app.put("/api/tasks/:id", async (c) => {
   const id = parseInt(c.req.param().id);
   const delta = await c.req.json();
   tasks[id] = { ...tasks[id], ...delta };
-  return c.status(200);
+  return c.newResponse(null, 200);
 });
 serve(app);
