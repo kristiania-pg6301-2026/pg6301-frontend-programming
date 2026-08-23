e54a988acd3e8e055f0342c3026bff4baeb5fe83
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 22:49:25 +0200

    serve hardcoded JSON from /api/tasks

--- server/server.js
+++ server/server.js
@@ -2,4 +2,12 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
 const app = new Hono();
+app.get("/api/tasks", (c) =>
+  c.json([
+    { title: "Create client app", completed: true },
+    { title: "Fetch from server", completed: true },
+    { title: "Post to server", completed: false },
+    { title: "Deploy to clever.cloud", completed: false },
+  ]),
+);
 serve(app);
