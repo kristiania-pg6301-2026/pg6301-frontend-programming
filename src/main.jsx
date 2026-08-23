2677b1efca236f8667431c451f4488cdaf9f6a4b
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 22:36:29 +0200

    useState to capture the task name

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 
 function Application() {
+  const [newTask, setNewTask] = useState("");
+
   return (
     <>
       <h1>My tasks</h1>
@@ -10,10 +13,14 @@ function Application() {
       <form>
         <div>
           <label>Task: </label>
-          <input type="text" />
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
         </div>
         <div>
-          <button>Save task</button>
+          <button>Save {newTask}</button>
         </div>
       </form>
 
