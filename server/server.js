e1605948f39a85ac2d1819bc429599a968701097
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 22:56:52 +0200

    server needs ids for tasks

--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ import { serve } from "@hono/node-server";
 
 const app = new Hono();
 const tasks = [
-  { title: "Create client app", completed: true },
-  { title: "Fetch from server", completed: true },
-  { title: "Post to server", completed: true },
-  { title: "Update on server", completed: false },
-  { title: "Deploy to clever.cloud", completed: false },
+  { id: 0, title: "Create client app", completed: true },
+  { id: 1, title: "Fetch from server", completed: true },
+  { id: 2, title: "Post to server", completed: true },
+  { id: 3, title: "Update on server", completed: false },
+  { id: 4, title: "Deploy to clever.cloud", completed: false },
 ];
 app.get("/api/tasks", (c) => c.json(tasks));
 app.post("/api/tasks", async (c) => {
   const task = await c.req.json();
-  tasks.push(task);
+  tasks.push({ ...task, id: tasks.length });
   return c.send(200);
 });
 serve(app);
