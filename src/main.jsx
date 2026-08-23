f33b539de8732ce652f2fecd4c2f2e3eb24054ac
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 22:37:40 +0200

    create a handler (handleSubmit) for an event (onSubmit)

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import { useState } from "react";
 function Application() {
   const [newTask, setNewTask] = useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Submitting!");
+  }
+
   return (
     <>
       <h1>My tasks</h1>
 
       <h2>Create new tasks</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Task: </label>
           <input
