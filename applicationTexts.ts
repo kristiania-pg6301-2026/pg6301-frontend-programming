46b62fefa990e11d6bc5aedc72cf29281f0a208e
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 20:46:14 +0200

    🔵 implement TypeScript types for application messages

--- applicationTexts.ts
+++ applicationTexts.ts
@@ -1,12 +1,20 @@
-export const norwegian = {
+interface ApplicationTexts {
+  generalError: string;
+  networkError: string;
+}
+
+export const norwegian: ApplicationTexts = {
   generalError: "Noe gikk feil",
   networkError: "Mistet kontakt med serveren",
 };
-export const english = {
+export const english: ApplicationTexts = {
   generalError: "An error has occurred",
   networkError: "Failed to connect to server",
 };
 
-export function applicationTexts(language: any, error: string) {
+export function applicationTexts(
+  language: ApplicationTexts,
+  error: keyof ApplicationTexts,
+) {
   return language[error];
 }
