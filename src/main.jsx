8b02bc9fe8326aa5bebfc030a486fc922df48010
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 22:35:19 +0200

    create task form

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,25 @@
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("app")).render(<h1>Hello React</h1>);
+function Application() {
+  return (
+    <>
+      <h1>My tasks</h1>
+
+      <h2>Create new tasks</h2>
+
+      <form>
+        <div>
+          <label>Task: </label>
+          <input type="text" />
+        </div>
+        <div>
+          <button>Save task</button>
+        </div>
+      </form>
+
+      <h2>Existing tasks</h2>
+    </>
+  );
+}
+
+createRoot(document.getElementById("app")).render(<Application />);
