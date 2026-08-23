45909687568efc0580bdfd4c7d98fdf667543584
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 20:39:02 +0200

    🔵 make language object containing error messages

--- applicationTexts.ts
+++ applicationTexts.ts
@@ -1,11 +1,12 @@
-export const norwegian = {};
-export const english = {};
+export const norwegian = {
+  generalError: "Noe gikk feil",
+  networkError: "Mistet kontakt med serveren",
+};
+export const english = {
+  generalError: "An error has occurred",
+  networkError: "Failed to connect to server",
+};
 
 export function applicationTexts(language: any, error: string) {
-  if (error === "networkError") {
-    if (language === english) return "Failed to connect to server";
-    return "Mistet kontakt med serveren";
-  }
-  if (language === english) return "An error has occurred";
-  return "Noe gikk feil";
+  return language[error];
 }
