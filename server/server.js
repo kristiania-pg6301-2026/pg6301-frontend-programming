9f8e2c2a1687ef324d86f02583d0194e1b0ddc91
Johannes Brodwall <jhannes@gmail.com>
2026-08-23 22:53:14 +0200

    server handles POST requests

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
 const app = new Hono();
-app.get("/api/tasks", (c) =>
-  c.json([
-    { title: "Create client app", completed: true },
-    { title: "Fetch from server", completed: true },
-    { title: "Post to server", completed: false },
-    { title: "Deploy to clever.cloud", completed: false },
-  ]),
-);
+const tasks = [
+  { title: "Create client app", completed: true },
+  { title: "Fetch from server", completed: true },
+  { title: "Post to server", completed: true },
+  { title: "Update on server", completed: false },
+  { title: "Deploy to clever.cloud", completed: false },
+];
+app.get("/api/tasks", (c) => c.json(tasks));
+app.post("/api/tasks", async (c) => {
+  const task = await c.req.json();
+  tasks.push(task);
+  return c.send(200);
+});
 serve(app);
