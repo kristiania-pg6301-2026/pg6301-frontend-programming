df77fd2dd642d4fc55c530a3e4c0eaf1b9237d5f
Johannes Brodwall <jhannes@gmail.com>
2026-08-27 12:45:03 +0200

    🟢 test("network error in both languages")

--- applicationTexts.test.js
+++ applicationTexts.test.js
@@ -4,7 +4,11 @@ const norwegian = {};
 const english = {};
 
 function showText(errorCode, language) {
-  if (language === norwegian) return "En feil har inntruffet";
+  if (language === norwegian) {
+      if (errorCode === "networkError") return "Mistet kontakt med serveren"
+      return "En feil har inntruffet";
+  }
+    if (errorCode === "networkError") return "Cannot communicate with the server"
   return "Something went wrong";
 }
 
@@ -12,3 +16,9 @@ test("general error in both languages", () => {
   expect(showText("generalError", norwegian)).toBe("En feil har inntruffet");
   expect(showText("generalError", english)).toBe("Something went wrong");
 });
+
+test("network error in both languages", () => {
+  expect(showText("networkError", norwegian)).toBe("Mistet kontakt med serveren");
+  expect(showText("networkError", english)).toBe("Cannot communicate with the server");
+});
+
