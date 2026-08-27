b0050f460dafa32f3c23fca3f9d60dd639934eb4
Johannes Brodwall <jhannes@gmail.com>
2026-08-27 12:43:06 +0200

    🟣 reformat with prettier

--- applicationTexts.test.js
+++ applicationTexts.test.js
@@ -1,16 +1,14 @@
-import {test, expect} from "vitest";
+import { test, expect } from "vitest";
 
 const norwegian = {};
 const english = {};
 
 function showText(errorCode, language) {
-    if (language === norwegian) return "En feil har inntruffet";
-    return "Something went wrong";
+  if (language === norwegian) return "En feil har inntruffet";
+  return "Something went wrong";
 }
 
 test("general error in both languages", () => {
-    expect(showText("generalError", norwegian))
-        .toBe("En feil har inntruffet");
-    expect(showText("generalError", english))
-        .toBe("Something went wrong");
-})
+  expect(showText("generalError", norwegian)).toBe("En feil har inntruffet");
+  expect(showText("generalError", english)).toBe("Something went wrong");
+});
