330c869b50c5e878e513636dcc17015be10a2b97
Johannes Brodwall <jhannes@gmail.com>
2026-09-01 13:04:05 +0200

    Create new task

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, type SubmitEvent } from "react";
 import { createRoot } from "react-dom/client";
 
 function Application() {
-  const tasks = [
+  const [taskName, setTaskName] = useState("The new task");
+
+  const [tasks, setTasks] = useState([
     { name: "Create new project", completed: true },
     { name: "Add typescript and husky", completed: true },
     { name: "Show tasks", completed: true },
-    { name: "Create new task", completed: false },
+    { name: "Create new task", completed: true },
     { name: "Fetch tasks from server", completed: false },
     { name: "Create task on server", completed: false },
     { name: "Deploy application to Clever Cloud", completed: false },
     { name: "Update task on server", completed: false },
-  ];
+  ]);
+
+  function handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    setTasks((old) => [
+      ...old,
+      {
+        name: taskName,
+        completed: false,
+      },
+    ]);
+  }
 
   return (
     <>
@@ -24,6 +37,19 @@ function Application() {
           </li>
         ))}
       </ol>
+      <h2>Create new task</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Task name:{" "}
+          <input
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+        </div>
+        <div>
+          <button>Save {taskName}</button>
+        </div>
+      </form>
     </>
   );
 }
