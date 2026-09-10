4fb9ba46d92ebd18f87243c280ad3c1452609e68
Johannes Brodwall <jhannes@gmail.com>
2026-09-10 09:35:59 +0200

    show tasks

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,22 @@
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 
-createRoot(document.getElementById("app")).render(<h1>Hello World</h1>);
+function Application() {
+  const [tasks, setTasks] = useState([
+    { description: "create npm app" },
+    { description: "create react app" },
+  ]);
+
+  return (
+    <>
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>{t.description}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+createRoot(document.getElementById("app")).render(<Application />);
