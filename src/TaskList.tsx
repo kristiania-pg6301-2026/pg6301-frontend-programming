98d1162bfc0de41b7252791a426b9c28533aa624
Johannes Brodwall <jhannes@gmail.com>
2026-09-10 13:38:12 +0200

    mark tasks as done

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,11 +1,21 @@
 import type { TaskItem } from "./TaskItem.js";
 
-export default function TaskList({ tasks }: { tasks: TaskItem[] }) {
+export default function TaskList({
+  tasks,
+  onTaskCompleteUpdated,
+}: {
+  tasks: TaskItem[];
+  onTaskCompleteUpdated(task: TaskItem, completed: boolean): void;
+}) {
   return (
     <ul>
       {tasks.map((t) => (
         <li>
-          <input type={"checkbox"} checked={t.completed} />
+          <input
+            type={"checkbox"}
+            checked={t.completed}
+            onChange={(e) => onTaskCompleteUpdated(t, e.target.checked)}
+          />
           {t.description}
         </li>
       ))}
--- src/main.tsx
+++ src/main.tsx
@@ -8,17 +8,24 @@ function Application() {
   const [tasks, setTasks] = useState<TaskItem[]>([
     { description: "Show current tasks", completed: true },
     { description: "Create new tasks", completed: true },
-    { description: "Mark task as done", completed: false },
+    { description: "Mark task as done", completed: true },
   ]);
 
   function handleNewTask(task: TaskItem) {
     setTasks((old) => [task, ...old]);
   }
 
+  function handleTaskCompleteUpdated(task: TaskItem, completed: boolean) {
+    setTasks((old) => old.map((o) => (o === task ? { ...o, completed } : o)));
+  }
+
   return (
     <>
       <h1>My Tasks</h1>
-      <TaskList tasks={tasks} />
+      <TaskList
+        tasks={tasks}
+        onTaskCompleteUpdated={handleTaskCompleteUpdated}
+      />
       <h2>Create new task</h2>
       <NewTaskForm onNewTask={handleNewTask} />
     </>
