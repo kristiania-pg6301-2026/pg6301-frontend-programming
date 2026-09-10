b177b9ffc870f47f06fa2bc3a3c0dc6534ebd1c2
Johannes Brodwall <jhannes@gmail.com>
2026-09-10 09:53:37 +0200

    introduce type TaskItem for TaskList

--- src/taskList.tsx
+++ src/taskList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import type { TaskItem } from "./taskItem.js";
 
-export function TaskList({ tasks }: { tasks: any[] }) {
+export function TaskList({ tasks }: { tasks: TaskItem[] }) {
   return (
     <ul>
       {tasks.map((t) => (
