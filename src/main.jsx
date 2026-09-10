8dbfb4c772fbbbe1ab9166652d0e394efa66922f
Johannes Brodwall <jhannes@gmail.com>
2026-09-10 12:45:48 +0200

    create new task (with a bug!)

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 import TaskList from "./TaskList";
+import NewTaskForm from "./NewTaskForm";
 
 function Application() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { description: "create npm project" },
     { description: "create react code" },
-  ];
+  ]);
+
+  function handleNewTask(task) {
+    setTasks((old) => [task, ...old]);
+  }
+
   return (
     <>
       <h1>My Tasks</h1>
       <TaskList tasks={tasks} />
+      <h2>Create new task</h2>
+      <NewTaskForm onNewTask={handleNewTask} />
     </>
   );
 }
