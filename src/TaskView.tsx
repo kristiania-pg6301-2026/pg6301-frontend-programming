76c81926da4aa3fdeda1d373d8b61f9b6aa8bd43
Johannes Brodwall <jhannes@gmail.com>
2026-09-17 12:52:46 +0200

    edit task in a separate state

--- src/TaskView.tsx
+++ src/TaskView.tsx
@@ -9,25 +9,35 @@ export default function TaskView({
   onChangeTaskDetail(id: number, description: string): void;
 }) {
   const [details, setDetails] = useState(task.details || "");
+  const [isEditing, setIsEditing] = useState(false);
 
   function handleSubmit(event: SubmitEvent) {
     event.preventDefault();
     onChangeTaskDetail(task.id, details);
+    setIsEditing(false);
   }
 
   return (
     <>
       <h1>Task: {task.description}</h1>
 
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={details}
-          onChange={(e) => setDetails(e.target.value)}
-        />
-        <div>
-          <button>Save</button>
-        </div>
-      </form>
+      {task.details}
+
+      <div>
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+      </div>
+
+      {isEditing && (
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={details}
+            onChange={(e) => setDetails(e.target.value)}
+          />
+          <div>
+            <button>Save</button>
+          </div>
+        </form>
+      )}
     </>
   );
 }
