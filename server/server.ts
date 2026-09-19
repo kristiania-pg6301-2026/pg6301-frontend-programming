eb61824cc63199980f220c969ff1790f57053e1f
Johannes Brodwall <jhannes@gmail.com>
2026-09-19 13:47:44 +0200

    create Hono server

---
+++ server/server.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import type { TaskItem } from "../src/TaskItem.js";
+
+const app = new Hono();
+serve({ fetch: app.fetch, port: 8080 });
+const tasks: TaskItem[] = [
+  { id: 0, description: "Create project (server)", completed: true },
+  { id: 1, description: "Create React webapp (server)", completed: true },
+  { id: 2, description: "Create Hono backend", completed: true },
+  { id: 3, description: "Update with Hono backend", completed: false },
+];
+app.get("/api/tasks", (c) => {
+  return c.json(tasks);
+});
